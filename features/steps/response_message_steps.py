d9219392e04a6295054cec93d5b8b753590231ec
AndyDeany <oneandydean@hotmail.com>
2017-11-18 12:02:16 +0000

    Fix repsonse message steps regex to work with @ symbols

--- features/steps/response_message_steps.py
+++ features/steps/response_message_steps.py
@@ -24,7 +24,8 @@ EXPECTED_MESSAGES = {
 }
 
 
-@step("the response message should indicate that ((?:\w ?)+)")
+@step("the response message should indicate that ((?:[\w@] ?)+)")
 def check_message(self, message_purpose):
-    expected_message = EXPECTED_MESSAGES[message_purpose].format(**world.__dict__)
+    expected_message = EXPECTED_MESSAGES[message_purpose]
+    expected_message = expected_message.format(**world.__dict__)
     expect(world.response.json()["message"]).to(equal(expected_message))
