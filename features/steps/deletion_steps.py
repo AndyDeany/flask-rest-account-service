73e1cf0a57fcbb967b74c8bafd8bb37c5ba3b9a9
AndyDeany <oneandydean@hotmail.com>
2017-11-19 11:14:46 +0000

    Add implementation for deletion tests

---
+++ features/steps/deletion_steps.py
@@ -0,0 +1,30 @@
+"""Module containing steps for sending POSt requests to the /login endpoint."""
+from aloe import step, world
+from requests import get, delete
+
+from features.helper import *
+from features.helper.accounts_api import create_random_account
+
+
+@step(r"I try to delete a user's account")
+def delete_user_account(self):
+    create_random_account()
+    world.response = delete(url(f"/accounts/{world.username}"))
+
+
+@step(r"I try to delete all users' accounts")
+def delete_all_accounts(self):
+    create_random_account()
+    world.initial_accounts = get(url("/accounts")).json()
+    world.response = delete(url("/accounts"))
+
+
+@step(r"the user's account should be deleted")
+def check_user_account_deleted(self):
+    expect(get(url(f"/accounts/{world.response}")).status_code).to(equal(404))
+
+
+@step(r"no accounts should be deleted")
+def check_response_login_status(self):
+    accounts = get(url("/accounts")).json()
+    expect(accounts).to(contain(*world.initial_accounts))
